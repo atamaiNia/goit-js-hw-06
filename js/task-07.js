a33c88ee8776a00565b0f93bec495a3bcb5ecca1
atamaiNia <bimatideka@gmail.com>
2022-07-25 14:35:26 +0300

    task-7 completed

---
+++ js/task-07.js
@@ -0,0 +1,18 @@
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+// <input id="font-size-control" type="range" min="16" max="96" />
+// <br />
+// <span id="text">Abracadabra!</span>
+
+const refs = {
+  input: document.querySelector("#font-size-control"),
+  spanText: document.querySelector("#text")
+};
+
+function onChangeFontSize() {
+  refs.spanText.style.fontSize = refs.input.value + "px";
+  //   console.log(refs.spanText.style.fontSize);
+}
+
+refs.input.addEventListener("input", onChangeFontSize);
+// console.log(onChangeFontSize());
